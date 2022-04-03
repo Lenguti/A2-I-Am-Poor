d479504250cb83bbb68f61030e6c8dae9d334973
Lenard <lenard@buzzer.com>
2022-04-03 12:59:17 -0400

    Initial Commit

---
+++ A2-I-Am-Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2-I-Am-Poor
+//
+//  Created by Lenard Gutierrez on 4/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
